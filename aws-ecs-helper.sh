4042fe21a44a530b840c9df9f2adc8ab2968d15c
Alex Iudice <alex.iudice@atomrain.com>
2020-01-31 11:28:23 -0500

    Added ec_graph_ip() function for getting neo4j instance container IPs through terminal. Renamed ecs_ip() to ec_service_ip().

--- aws-ecs-helper.sh
+++ aws-ecs-helper.sh
@@ -10,19 +10,28 @@
 # 
 # 
 # Usage:
-#    ecs_ip <cluster> <service>
+#    ec_service_ip <cluster> <service>
+#    ec_graph_ip <cluster> <graphAbbreviationName>
 # 
 # 
 # Ex:
-#  ecs_ip vive-stag search
+#  ec_service_ip vive-stag search
 #
 #   Output: 
 #     172.26.34.4
 #     172.26.34.118
+#
+# Ex:
+#  ec_graph_ip vive-stag ongdb
+#
+#   Output: 
+#     172.26.36.12
+
+
 
 # $1 = cluster (vive-stag, gg-dev, etc.)
 # $2 = service name (search, nlp, fuze, etc.)
-function ecs_ip () {
+function ec_service_ip () {
   cluster="$1-frazr-core"
   service="$1-$2"
   taskArns=$(aws ecs list-tasks --output text --query taskArns[*] --cluster "$cluster" --service-name "$service" | awk '{print $1 " " $2}')
@@ -33,3 +42,14 @@ function ecs_ip () {
 
   aws ec2 describe-instances --output text --query Reservations[*].Instances[*].PrivateIpAddress --instance-ids $(echo "$ec2InstanceIds")
 }
+
+
+# $1 = cluster (vive-stag, gg-dev, etc.)
+# $2 = graph abbreviation name (neo, ongdb)
+function ec_graph_ip () {
+  tagValue="$1-$2"
+  filters="Name=tag:Cluster,Values=$tagValue"
+  aws ec2 describe-instances --output text --query Reservations[*].Instances[*].PrivateIpAddress  --filters $(echo "$filters")
+}
+
+
