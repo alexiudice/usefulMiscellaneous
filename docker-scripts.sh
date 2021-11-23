de743c15922405b2decf0b0381e6085a83b6c48d
Alex Iudice <alex.iudice@atomrain.com>
2021-11-23 14:11:08 -0500

    Removes poorly named 'mvn_run' with 'docker_run' local function.

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -11,3 +11,7 @@ function docker_stop_containers () {
 function docker_rm_containers () {
   docker container rm $(docker container ls -aq)
 }
+
+function docker_run () {
+  docker-compose up --detach --build
+}
--- maven-scripts.sh
+++ maven-scripts.sh
@@ -8,9 +8,10 @@ function mvn_build () {
   mvn clean install -Pgraphgrid,build-project,build-docker -DskipTests
 }
 
-function mvn_run () {
-  docker-compose up --detach --build
-}
+# Replaced with docker_run
+# function mvn_run () {
+#   docker-compose up --detach --build
+# }
 
 function mvn_quick () {
   mvn clean install -Pgraphgrid,build-project,build-docker -DskipTests \
